2aac0fc7c16bde460c1c94e291d777d16ede9059
Shasta <saritchi@uwaterloo.ca>
2020-12-21 11:26:31 -0500

    formatting

--- picturesque/storage.py
+++ picturesque/storage.py
@@ -2,22 +2,22 @@ from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 import os
 
-def add_picture_to_storage(user, storage):
 
+def add_picture_to_storage(user, storage):
     # Open dialog to select image
     Tk().withdraw()
     filepath = askopenfilename()
 
     # Get name of image
     filename, fileext = os.path.splitext(filepath)
-    filename = filename.split('/')[len(filename.split('/'))-1]
+    filename = filename.split('/')[len(filename.split('/')) - 1]
     try:
         # Verify the file is an image (jpg or png)
         if fileext != ".jpg" and fileext != ".png":
             raise TypeError
         # Upload image to repository
-        id = user['localId']
-        storage.child("user/{}/{}".format(id, filename)).put(filepath, user['idToken'])
+        user_id = user['localId']
+        storage.child("user/{}/{}".format(user_id, filename)).put(filepath, user['idToken'])
         print("File {} uploaded successfully!".format(filename))
     except TypeError:
         print("File is not a .jpg or .png. Please try again.")
@@ -25,9 +25,8 @@ def add_picture_to_storage(user, storage):
         print("Something went wrong. File not uploaded.")
 
 
-
 def storage_loop(user, storage, db):
-    while (True):
+    while True:
         print("Options: 1 - Upload file, 2 - Search tag, 3 - Search filename, 4 - Quit")
         answer = input("Enter one of the options above: ")
         if int(answer) == 1:
@@ -37,4 +36,3 @@ def storage_loop(user, storage, db):
             return
         else:
             print("Not implemented yet.")
-
