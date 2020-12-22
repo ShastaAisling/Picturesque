9c5713824d548448240a317c10d13e18f8df7b91
Shasta <saritchi@uwaterloo.ca>
2020-12-21 21:21:18 -0500

    set up AI tagging

--- picturesque/storage.py
+++ picturesque/storage.py
@@ -1,16 +1,20 @@
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
+from picturesque.tagging import tag_image
 import os
 
 def add_picture_to_storage(user, storage):
-
     # Open dialog to select image
     Tk().withdraw()
     filepath = askopenfilename()
 
     # Get name of image
     filename, fileext = os.path.splitext(filepath)
-    filename = filename.split('/')[len(filename.split('/'))-1]
+    filename = filename.split('/')[len(filename.split('/')) - 1]
+
+    # Get tag for image
+    tag = tag_image(filepath)
+
     try:
         # Verify the file is an image (jpg or png)
         if fileext != ".jpg" and fileext != ".png":
@@ -25,9 +29,8 @@ def add_picture_to_storage(user, storage):
         print("Something went wrong. File not uploaded.")
 
 
-
 def storage_loop(user, storage, db):
-    while (True):
+    while True:
         print("Options: 1 - Upload file, 2 - Search tag, 3 - Search filename, 4 - Quit")
         answer = input("Enter one of the options above: ")
         if int(answer) == 1:
@@ -37,4 +40,3 @@ def storage_loop(user, storage, db):
             return
         else:
             print("Not implemented yet.")
-
---
+++ picturesque/tagging.py
@@ -0,0 +1,16 @@
+from imageai.Prediction import ImagePrediction
+import os
+
+execution_path = os.getcwd()
+prediction = ImagePrediction()
+prediction.setModelTypeAsResNet()
+prediction.setModelPath(execution_path + "/resnet50_weights_tf_dim_ordering_tf_kernels.h5")
+prediction.loadModel()
+
+
+def tag_image(filepath):
+    predictions, percentage_probabilities = prediction.predictImage(filepath, result_count=5)
+    for index in range(len(predictions)):
+        print(predictions[index], " : ", percentage_probabilities[index])
+
+    return predictions[0]
